82cc44a9d79dbc7c07e5a9d46c8fae5ea1b9286b
Petr Kulhanek <kulhanek@chemi.muni.cz>
2016-07-24 00:05:25 +0200

    bug fix

--- templates/login.php
+++ templates/login.php
@@ -17,7 +17,7 @@ if( preg_match("/[^A-Za-z0-9]/", $username) ) {
 }
 printf("<input class=\"text\" type=\"text\" placeholder=\"Přezdívka\" name=\"username\" value=\"%s\"",$username);
 ?>
-onkeydown="javascript:if (event.keyCode == 13) document.getElementById('password').focus(); window.event.returnValue = false;" />
+onkeydown="javascript:if (event.keyCode == 13) document.getElementById('password').focus();" />
     <input id="password" class="text" type="password" placeholder="Heslo" name="skey" onkeydown="javascript:if (event.keyCode == 13) do_action('login');"/>
     <input class="flat" type="button" name="login" value="Přihlásit se" onclick="do_action('login');"/>
 </div>
