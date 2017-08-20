a0b7cad0a605bd9b145836e307491389b2915664
Petr Kulhanek <kulhanek@chemi.muni.cz>
2017-08-20 21:51:36 +0200

    stat - added

--- templates/stat.php
+++ templates/stat.php
@@ -1,17 +1,39 @@
 <table>
     <tr>
-        <td>Hráč:</td>
-        <td>Účast:</td>
+        <td>Hráč</td>
+        <td>Účast</td>
     </tr>
 <?php
     $users = file(sprintf("data/%s.users",$_SESSION['season']));
     
+    $allnatt = 0;
     foreach($users as $user) {
         $user = str_replace(array("\n"), '', $user);
         $natt = 0;
+        
+        $handle = fopen(sprintf("data/%s",$_SESSION['season']),"r");
+        if( ! $handle ) break;
+
+        while( ($line = fgets($handle)) !== false ) {        
+            $pid = strtok($line, "\t"); // play id
+            if( is_file(sprintf("var/%s.%d.%s",$_SESSION['season'],$pid,$user)) ){
+                $handle = fopen(sprintf("var/%s.%d.%s",$_SESSION['season'],$pid,$user),"r");
+                fscanf($handle,"%d",$flag);
+                if( $flag == 1 ){
+                    $natt++;
+                    $allnatt++;
+                }
+                fclose($handle);
+            }
+        }
+        
         printf("<tr>\n");
         printf("    <td>%s</td><td>%d</td>\n",$user,$natt);
         printf("</tr>\n");        
     }
+    
+        printf("<tr>\n");
+        printf("    <td>Celkem:</td><td>%d</td>\n",$allnatt);
+        printf("</tr>\n");     
 ?>
 </table>
