f495b30730c878848be0c9a97545d6d8c25bb569
Petr Kulhanek <kulhanek@chemi.muni.cz>
2017-08-20 22:02:03 +0200

    stat - added

--- templates/stat.php
+++ templates/stat.php
@@ -1,12 +1,42 @@
+<?php
+    $price=3710;
+?>
+
 <table>
     <tr>
         <td>Hráč</td>
         <td>Účast</td>
+        <td>Příspěvek</td>
     </tr>
 <?php
     $users = file(sprintf("data/%s.users",$_SESSION['season']));
     
     $allnatt = 0;
+    foreach($users as $user) {
+        $user = str_replace(array("\n"), '', $user);
+        
+        $handle = fopen(sprintf("data/%s",$_SESSION['season']),"r");
+        if( ! $handle ) break;
+
+        while( ($line = fgets($handle)) !== false ) {        
+            $pid = strtok($line, "\t"); // play id
+            if( is_file(sprintf("var/%s.%d.%s",$_SESSION['season'],$pid,$user)) ){
+                $handle1 = fopen(sprintf("var/%s.%d.%s",$_SESSION['season'],$pid,$user),"r");
+                fscanf($handle1,"%d",$flag);
+                if( $flag == 1 ){
+                    $allnatt++;
+                }
+                fclose($handle1);
+            }
+        }
+        
+        fclose($handle);  
+    }
+
+    $
+
+    $users = file(sprintf("data/%s.users",$_SESSION['season']));
+    
     foreach($users as $user) {
         $user = str_replace(array("\n"), '', $user);
         $natt = 0;
@@ -30,12 +60,12 @@
         fclose($handle);
         
         printf("<tr>\n");
-        printf("    <td>%s</td><td>%d</td>\n",$user,$natt);
+        printf("    <td>%s</td><td>%d</td><td>%d</td>\n",$user,$natt,$price*$natt/$allnatt);
         printf("</tr>\n");        
     }
     
         printf("<tr>\n");
-        printf("    <td>Celkem:</td><td>%d</td>\n",$allnatt);
+        printf("    <td>Celkem:</td><td>%d</td><td>%d</td>\n",$allnatt,$price);
         printf("</tr>\n");     
 ?>
 </table>
