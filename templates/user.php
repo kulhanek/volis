cb746013fd94f9aeb28af1b77001772a1694bc56
Petr Kulhanek <kulhanek@chemi.muni.cz>
2017-08-20 21:13:08 +0200

    registerd users - added

--- templates/user.php
+++ templates/user.php
@@ -3,7 +3,7 @@ printf("<h1>Sezóna: %s</h1>\n",get_season_title($_SESSION['season']));
 ?>
 <div id="player">
 <?php 
-    printf('<h2 class="player"><span>Hráč: %s</span><input class="flat" type="button" name="logout" value="Statistika" onclick="do_action(\'stat\');"/><input class="flat" type="button" name="logout" value="Odhlásit se" onclick="do_action(\'logout\');"/></h2>',$_SESSION['username']);
+    printf('<h2 class="player"><span>Hráč: %s</span><input class="flat" type="button" name="logout" value="Statistika" onclick="do_action(\'stat\');"/> <input class="flat" type="button" name="logout" value="Odhlásit se" onclick="do_action(\'logout\');"/></h2>',$_SESSION['username']);
 ?>
 </div>
 <div style="clear: both;"></div>
